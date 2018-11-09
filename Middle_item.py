a072032bfce86de4d5cd1684f698444c0ed5310e
mrgrag <gm9218@yahoo.com>
2018-11-08 18:37:42 -0600

    Revise Middle_item

--- Middle_item.py
+++ Middle_item.py
@@ -8,3 +8,5 @@ def middle_element(lst):
 
 #Uncomment the line below when your function is done
 print(middle_element([5, 2, -10, -4, 4, 5]))
+#Create a function called middle_element that has one parameter named lst.
+#If there are an odd number of elements in lst, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
